35ecc5cfe12ccc07b21f3e13020fafb6e3f5bced
Agbroko David Oghenedoro <sirdoro1@gmail.com>
2019-06-06 11:48:59 +0100

    delete button working for the todo list

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,48 @@
-function chat(){
-    var node = document.createElement("LI");               //Create the list element
-    var delbtn = document.createElement("BUTTON");
-    var donebtn = document.createElement("BUTTON"); 
-    var delattr = document.createAttribute("class");
-    var doneattr = document.createAttribute("class");
-    var deltxt = document.createTextNode(<i class='fa fa-check-circle'></i>);
-    var donetxt = document.createTextNode(<i class='fa fa-trash'></i>);
-    delattr.value = "btn delbtn";
-    doneattr.value = "btn donebtn";
-    delbtn.setAttributeNode(delattr);
-    donebtn.setAttributeNode(doneattr);
-    delbtn.appendChild(deltxt);
-    donebtn.appendChild(donetxt);
-    var msg = document.getElementById("message").value;     //The String value from the Input
-    var textnode = document.createTextNode(msg);            //Creating TextNode with the String from the input
-    node.appendChild(textnode);                             // Appending the String to the Created list element
-    node.appendChild(delbtn);
-    node.appendChild(donebtn);
-    var att = document.createAttribute("class");            //Creating attrinbute for the created list
-    att.value = "list-group-item";                          // Value of the attribute
-    node.setAttributeNode(att);                             //Attaching the attribute to the list element
-    document.getElementById("uncompletedlist").appendChild(node); // Appending the list element to the UL(Unordered List Element)
-}
+
+    var delHTML = "<i class='fa fa-trash'></i>";
+    var doneHTML = "<i class='fa fa-check-circle'></i>";
+
+    function chat(){
+        
+        var msg = document.getElementById("message").value;  
+        var ul = document.getElementById("todo");
+
+
+        // Delete Button
+        var deletebtn = document.createElement("BUTTON");
+        deletebtn.innerHTML = delHTML;
+        deletebtn.classList.add('btn','remove');
+        deletebtn.addEventListener('click',removetask);
+
+
+        // Done Button
+        var donebtn = document.createElement("BUTTON"); 
+        donebtn.innerHTML = doneHTML;
+        donebtn.classList.add('btn','complete');
+
+        
+        // Buttons Span
+        var btnspan = document.createElement("SPAN");
+        btnspan.classList.add('listbutton','buttons');
+        btnspan.appendChild(deletebtn);
+        btnspan.appendChild(donebtn);
+
+
+        // LI ELEMENT
+        var node = document.createElement("LI");              
+        node.innerText = msg;  
+        node.classList.add('list-group-item')                                  
+        node.appendChild(btnspan);                            
+        ul.insertBefore(node,ul.childNodes[0]); 
+    }
+
+    function removetask(e){
+        var child = this.parentNode.parentNode;
+        var parent = child.parentNode;
+
+        parent.removeChild(child);
+    }
+
+
+    
+    
